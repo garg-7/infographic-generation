9fd707719d4ab7952a1ecf526dcb865baae69a96
Tanuj22 <agarwal.5@iitj.ac.in>
2021-01-17 02:18:40 +0530

    changes
    Signed-off-by: Tanuj22 <agarwal.5@iitj.ac.in>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from "react";
 import Container from "@material-ui/core/Container";
 import styled from 'styled-components';
+import Infograpgic from "./Components/infographics";
 
 const AppWrapper = styled.div`
-  .layout{
-    background-color: ${props=> props.backgroundColor};
-    color: ${props=> props.textColor};
-  }
+  
 `;
 
 const App = () => {
     let backgroundColor = "#f45454";
     let textColor = "#f4eac7";
+    let value = "36%";
+    let text = "of the great lakes lie within Canadian territory";
+    let countryCode = "ca";
     return (
-        <AppWrapper backgroundColor={backgroundColor} textColor={textColor}>
-            <Container>
-                <div className="layout">
-
-                </div>
-            </Container>
-        </AppWrapper>
+        <Container>
+            <AppWrapper >
+                <Infograpgic
+                    backgroundColor={backgroundColor}
+                    textColor={textColor}
+                    value={value}
+                    text={text}
+                    code={countryCode}
+                />
+            </AppWrapper>
+        </Container>
     );
 };
 
---
+++ src/Components/infographics.js
@@ -0,0 +1,76 @@
+import React from "react";
+import styled from 'styled-components';
+
+const InfographicsWrapper = styled.div`
+    margin: 20px auto;
+  .layout{
+    background-color: ${props=> props.backgroundColor};
+    color: ${props=> props.textColor};
+    width: 500px;
+    height: 500px;
+    padding: 15px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    
+    .inner-layout{
+      margin: 0 auto;
+      height: 96%;
+      width: 96%;
+      border: ${props=> props.textColor} solid 4px;
+      border-radius: 10px;
+    }
+  }
+`;
+
+
+
+const Icon = ({ name, ...rest }) => {
+    const ImportedIconRef = React.useRef(null);
+    const [loading, setLoading] = React.useState(false);
+
+    React.useEffect(() => {
+        console.log("Hello")
+        setLoading(true);
+        const importIcon = async () => {
+            try {
+                ImportedIconRef.current = (await import(`!!@svgr/webpack?-svgo,+titleProp,+ref!../assets/maps/${name}/vector.svg`)).default;
+            } catch (err) {
+                // Your own error handling logic, throwing error for the sake of
+                // simplicity
+                throw err;
+            } finally {
+                setLoading(false);
+            }
+        };
+        importIcon();
+    }, [name]);
+
+    if (!loading && ImportedIconRef.current) {
+        const { current: ImportedIcon } = ImportedIconRef;
+        return <ImportedIcon {...rest} />;
+    }
+
+    return null;
+};
+
+const Infograpgic = (props) => {
+    return(
+        <InfographicsWrapper backgroundColor={props.backgroundColor} textColor={props.textColor}>
+            <div className="layout">
+                <div className="inner-layout">
+                    <div className="text-section">
+                        <span>{props.value}</span>
+                        {props.text}
+                    </div>
+                    <div className="map-section">
+                        <Icon name={props.code}/>
+                        adada
+                    </div>
+                </div>
+            </div>
+        </InfographicsWrapper>
+    )
+};
+
+export default Infograpgic;
