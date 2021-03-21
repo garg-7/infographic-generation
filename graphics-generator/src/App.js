2f862f4e68c57417f9d98d5bce77fcae29da72fd
Tanuj22 <agarwal.5@iitj.ac.in>
2021-03-22 02:41:30 +0530

    axios and post request
    Signed-off-by: Tanuj22 <agarwal.5@iitj.ac.in>

--- graphics-generator/src/App.js
+++ graphics-generator/src/App.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import Container from "@material-ui/core/Container";
 import styled from 'styled-components';
+import TextField from '@material-ui/core/TextField';
 import Infograpgic1 from "./Components/infographics1";
 import Infograpgic2 from "./Components/infographics2";
 import Infograpgic3 from "./Components/infographics3";
+import {Button} from "@material-ui/core";
+import axios from "axios";
 
 const AppWrapper = styled.div`
   font-family: 'Roboto', sans-serif;
+  .textfield{
+        margin-top: 100px;
+        width: 100%;
+        display: flex;
+        #outlined-basic{
+          width: 700px;
+        }
+    }
 `;
 
 const App = () => {
@@ -16,9 +27,38 @@ const App = () => {
     let text = "Decrease in forest cover of Australia in 12 years";
     let countryCode = "au";
     let element = "tree";
+
+    const [input, setInput] = useState('');
+    const [tokens, setTokens] = useState({})
+    const handleSubmit = evt => {
+        evt.preventDefault();
+        console.log(input)
+        axios.post("/tokenize", {text: input})
+            .then((res) => {
+                setTokens(res.data);
+            })
+
+    }
+
     return (
         <Container>
             <AppWrapper >
+                <form className="textfield" noValidate autoComplete="off" onSubmit={handleSubmit}>
+                    <TextField
+                        id="outlined-basic"
+                        label="text"
+                        variant="outlined"
+                        value={input}
+                        onInput={ e=>setInput(e.target.value)}
+                    />
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                    >
+                        Submit
+                    </Button>
+                </form>
                 <Infograpgic1
                     backgroundColor={backgroundColor}
                     textColor={textColor}
