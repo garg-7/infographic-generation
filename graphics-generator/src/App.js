26e3f7946871e3dec6e1101c8a025e3c0ae7d83a
Tanuj22 <agarwal.5@iitj.ac.in>
2021-03-22 05:48:38 +0530

    add states
    Signed-off-by: Tanuj22 <agarwal.5@iitj.ac.in>

--- graphics-generator/src/App.js
+++ graphics-generator/src/App.js
@@ -7,6 +7,10 @@ import Infograpgic2 from "./Components/infographics2";
 import Infograpgic3 from "./Components/infographics3";
 import {Button} from "@material-ui/core";
 import axios from "axios";
+import countries from "i18n-iso-countries"
+
+
+// India saw a 63 . 1 % hike in the per capita power consumption over the course of the past 12 months .
 
 const AppWrapper = styled.div`
   font-family: 'Roboto', sans-serif;
@@ -28,24 +32,40 @@ const colors = {
     "family" : { backgroundColor : "#38d9f2", textColor : "#afe5ed"},
 }
 
+const elements = {
+    "per capita power consumption": "lightning",
+    "forest-cover": "tree",
+    "gdp": "money",
+    "internet": "internet",
+    "population": "family"
+}
+
 
 
 const App = () => {
-    let backgroundColor = "#4287f5";  // #e31b47
-    let textColor = "#b0ceff";  // #e0abb6
-    let value = "15%";
-    let text = "Decrease in forest cover of Australia in 12 years";
-    let countryCode = "au";
-    let element = "internet";
-
     const [input, setInput] = useState('');
-    const [tokens, setTokens] = useState({})
+    const [countryCode, setCountryCode] = useState("")
+    const [value, setValue] = useState("")
+    const [element, setElement] = useState("")
+    const [backgroundColor, setBackgroundColor] = useState("")
+    const [textColor, setTextColor] = useState("")
+    const [text, setText] = useState("")
+
     const handleSubmit = evt => {
         evt.preventDefault();
         console.log(input)
         axios.post("/tokenize", {text: input})
             .then((res) => {
-                setTokens(res.data);
+                const data = res.data
+                console.log(data)
+                console.log(elements[data.parameter])
+                console.log(countries.getAlpha2Code("India", "en"));
+                // setCountryCode(countries.getAlpha2Code(data.country, "en"))
+                // setValue(data.value)
+                // setElement(elements[data.parameter])
+                // setBackgroundColor(colors[elements[data.parameter]].backgroundColor)
+                // setTextColor(colors[elements[data.parameter]].textColor)
+                // setText(`Decrease in ${data.parameter} of ${data.country} in ${data.time} `)
             })
     }
 
@@ -68,32 +88,38 @@ const App = () => {
                         Submit
                     </Button>
                 </form>
-                <Infograpgic1
-                    backgroundColor={backgroundColor}
-                    textColor={textColor}
-                    value={value}
-                    text={text}
-                    code={countryCode}
-                    element={element}
-                />
-                <Infograpgic2
-                    backgroundColor={backgroundColor}
-                    textColor={textColor}
-                    value={value}
-                    text={text}
-                    code={countryCode}
-                    element={element}
-                    decrease={true}
-                />
-                <Infograpgic3
-                    backgroundColor={backgroundColor}
-                    textColor={textColor}
-                    value={value}
-                    text={text}
-                    code={countryCode}
-                    element={element}
-                    decrease={true}
-                />
+                {
+                    text && (
+                        <>
+                            <Infograpgic1
+                                backgroundColor={backgroundColor}
+                                textColor={textColor}
+                                value={value}
+                                text={text}
+                                code={countryCode}
+                                element={element}
+                            />
+                            <Infograpgic2
+                                backgroundColor={backgroundColor}
+                                textColor={textColor}
+                                value={value}
+                                text={text}
+                                code={countryCode}
+                                element={element}
+                                decrease={true}
+                            />
+                            <Infograpgic3
+                                backgroundColor={backgroundColor}
+                                textColor={textColor}
+                                value={value}
+                                text={text}
+                                code={countryCode}
+                                element={element}
+                                decrease={true}
+                            />
+                        </>
+                    )
+                }
             </AppWrapper>
         </Container>
     );
