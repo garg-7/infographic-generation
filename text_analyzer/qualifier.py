0ae80385514d97af7c8bf14a6f138f1ecff12a85
Tanuj22 <agarwal.5@iitj.ac.in>
2021-03-24 04:04:16 +0530

    format and gitignore
    Signed-off-by: Tanuj22 <agarwal.5@iitj.ac.in>

--- text_analyzer/qualifier.py
+++ text_analyzer/qualifier.py
@@ -1,13 +1,14 @@
 import numpy as np
 
+
 def cosineSim(vec1, vec2):
-    dot_p = np.dot(vec1, vec2)/(np.linalg.norm(vec1) * np.linalg.norm(vec2))
-    if dot_p>1:
+    dot_p = np.dot(vec1, vec2) / (np.linalg.norm(vec1) * np.linalg.norm(vec2))
+    if dot_p > 1:
         dot_p = 1
-    return 1 - np.arccos(dot_p)/np.pi
+    return 1 - np.arccos(dot_p) / np.pi
+
 
 def getSimilarity(check_word):
-
     inc_vec = []
     dec_vec = []
     try:
@@ -23,14 +24,14 @@ def getSimilarity(check_word):
                     inc_vec = np.asarray(vec)
                     np.save('inc_vec.npy', inc_vec)
                     print("increase vector noted")
-                elif word == 'decrease':    
+                elif word == 'decrease':
                     vec = list(map(float, line[1:]))
                     dec_vec = np.asarray(vec)
                     np.save('dec_vec.npy', dec_vec)
                     print("decrease vector noted")
         # inc_vec = np.load('inc_vec.npy')
         # dec_vec = np.load('dec_vec.npy')
-    
+
     # print(inc_vec.shape)
 
     with open('wiki-news-300d-1M.vec', 'r') as f:
@@ -41,7 +42,7 @@ def getSimilarity(check_word):
                 vec = list(map(float, line[1:]))
                 vec = np.asarray(vec)
                 break
-    
+
     # get the similarity with increase and decrease
     inc_sim = cosineSim(vec, inc_vec)
     # print('with increase:', inc_sim)
@@ -49,9 +50,10 @@ def getSimilarity(check_word):
     # print('with decrease:', dec_sim)
 
     if inc_sim > dec_sim:
-        print(word,  'increase')
+        print(word, 'increase')
     else:
         print(word, 'decrease')
+
 
 if __name__ == '__main__':
     getSimilarity('hike')
